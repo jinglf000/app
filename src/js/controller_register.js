b8c55cfe22b8765739042080272f8dd4bc3c1e7b
jinglf000@gmail.com <jinglf000@gmail.com>
2017-01-08 23:31:16 +0800

    注册引导页 指令 。。。

--- src/js/controller_register.js
+++ src/js/controller_register.js
@@ -3,10 +3,6 @@
  */
 var register = angular.module("reg",[]);
 register.controller("regControl",['$scope',function ($scope) {
-    $scope.print = function(){
-        console.log($scope.regForm.user);
-        $scope.regForm.user += "xyz";
-    }
 
     // $scope.check_equal = function () {
     //     if($scope.regForm.pass.$modelValue != $scope.regForm.confirmPass.$modelValue){
@@ -14,18 +10,20 @@ register.controller("regControl",['$scope',function ($scope) {
     //         $scope.regForm.confirmPass.$error.no_equal = false;
     //     }
     // }
-}])
+}]);
+//这两种都是数据验证方式 各有优劣，先执行 $parse ---> $validators
+
 register.directive("checkUser",['$http','$q',function($http,$q){
     // email + cellPhone
     var valid_username = /^((1\d{10})|([-\w]?\w+([-+.]\w+)*[-\w]?@[-\w]?\w+([-.]\w+)*[-\w]?\.[-\w]?\w+([-.]\w+)*[-\w]?))$/;
     return {
         restrict: "A",
-        require : 'ngModel',
+        require : '?ngModel',
         link : function($scope,element,attrs,ctrl){
             // 同步验证 model值 和 view值
-            ctrl.$validators.username = function(modelValue,viewValue){
+            ctrl.$validators.checkUser = function(modelValue,viewValue){
                 var value = modelValue || viewValue;
-                if(!valid_username.test(value)){
+                if(!valid_username.test(value) && value){
                     return false;
                 }
                 return true;
@@ -33,3 +31,27 @@ register.directive("checkUser",['$http','$q',function($http,$q){
         }
     };
 }]);
+register.directive("oneToFifty",function($filter){
+    return {
+        require : "?ngModel",
+        link : function(scope,ele,attr,ngModel){
+            if(!ngModel) return;
+            ngModel.$parsers.unshift(function (viewValue) {
+                // if(ngModel.$error){return};
+                if(!viewValue){return viewValue};
+                var i = parseInt(viewValue);
+
+                if(i >= 0 && i <= 50 ){
+                    ngModel.$setValidity("oneToFifty",true);
+                    return viewValue;
+                }else{
+                    ngModel.$setValidity("oneToFifty",false);
+                    return undefined;
+                }
+            });
+            ngModel.$formatters.unshift(function(viewValue){
+               return $filter("number")(viewValue);
+            });
+        }
+    }
+});
